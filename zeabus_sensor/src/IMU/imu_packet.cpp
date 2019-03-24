80600769b37e5845bd4f60140b5252b68715a90e
SupasanKomonlit <supasan_224@hotmail.com>
2019-03-25 03:36:00 +0700

    Add packet source code

---
+++ zeabus_sensor/src/IMU/imu_packet.cpp
@@ -0,0 +1,103 @@
+// FILE         : imu_packet.cpp
+// AUTHOR       : Supasan Kommonlit
+// CREATE DATE  : 2019, MARCH 24
+
+#include    <zeabus_sensor/IMU/imu_packet.hpp>
+
+namespace zeabus
+{
+
+namespace sensor
+{
+namespace IMU
+{
+    Packet::Packet( unsigned int size )
+    {
+        this->reserve( size );
+    }
+
+     Packet::print_check_memory()
+    {
+        std::cout << "Now packet have number of element : " << (this->data).size() 
+                << "\n\tcapacity : " << (this->data).capacity() << "\n";
+    }
+
+    // Pattern is variadic fuction
+    template< typename type_single >
+    void Packet::push_data( type_single data_single )
+    {
+        (this->data).push_back( (unsigned char)data_single );
+    }
+
+    template< typename type_single , typename... type_pack >
+    void Packet::push_data( type_single data_single , type_pack data_pack )
+    {
+        push_data( data_single );
+        push_data( data_pack... );
+    }
+
+    bool Packet::check_sum( bool self_check )
+    {
+        unsigned int size = (this->data).size();
+        bool result = false;
+        if( self_check )
+        {
+            unsigned short int result = this->find_check_sum( size );
+        }
+        if( (this->data)[ size - 2 ] == (this->MSB) && (this->data)[ size - 1 ] == (this->LSB) )
+        {
+            result  = true;
+        }
+        return result;
+    }
+
+    void Packet::add_check_sum( )
+    {
+        this->find_check_sum( (this->data).size() );
+        this->push_data( this->MSB , this->LSB ); 
+    }
+
+    void Packet::find_check_sum( int last_data )
+    {
+        this->MSB = 0;
+        this->LSB = 0;
+        for( int  run_number = 0 ; run < last_data ; run++ )
+        {
+            (this->MSB) += (this->data)[run];
+            (this->LSB) += (this->MSB);
+        }
+    }
+
+    void Packet::clear_member()
+    {
+        (this->data).resize( 0 );
+    }
+
+    void Packet::fit_capacity()
+    {
+        (this->data).shrint_to_fit();
+    }
+
+    void Packet::resize( unsigned int size )
+    {
+        (this->data).resize( size );
+    }
+
+    void Packet::reserve( unsigned int size )
+    {
+        (this->data).reserve( size );
+    }
+
+    unsigned long int data_4_byte( int offset , bool result )
+    {
+        return ( (unsigned long int) (this->data)[offset] ) << 24 
+                | ( (unsigned long int ) (this->data)[offset + 1 ] << 16 )
+                | ( (unsigned long int ) (this->data)[offset + 2 ] << 8 )
+                | ( (unsigned long int ) (this->data)[offset + 3 ] );
+    }
+ 
+}
+
+}
+
+}
