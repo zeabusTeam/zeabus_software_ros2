951cb252674675398b88d3b9580cfb2fa5ab8d33
SupasanKomonlit <supasan_224@hotmail.com>
2019-04-01 20:08:01 +0700

    Add header file

---
+++ zeabus/include/zeabus/convert/bytes.hpp
@@ -0,0 +1,39 @@
+// FILE         : bytes.hpp
+// AUTHOR       : Supasan Komonlit
+// CREATE DATE  : 2019, APRIL 01
+
+#include    <iostream>
+
+#include    <cstring> // import for function memcpy
+
+// This file create for convert data bytes to variable type int / float or double up to you 
+// But ensure you have to study about size of type varaible and your data that are correct
+// for size of type varaible we reference from http://www.cplusplus.com/doc/tutorial/variables/
+
+#ifndef _ZEABUS_CONVERT_BYTES_HPP__
+#define _ZEABUS_CONVERT_BYTES_HPP__
+
+namespace zeabus
+{
+
+namespace convert
+{
+
+namespace bytes
+{
+    unsigned long int data32;
+    unsigned long long int data64;
+    
+    bool vector_to_floats( std::vector<unsigned char>* set_data 
+            , float* result , unsigned int offset = 0 );
+
+    bool vector_to_double( std::vector<unsigned char>* set_data
+            , double* result , unsigned int offset = 0 );
+
+} // namespace convert
+
+} // namespace convert
+
+} // namespace zeabus
+
+#endif // _ZEABUS_CONVERT_BYTES_HPP__
