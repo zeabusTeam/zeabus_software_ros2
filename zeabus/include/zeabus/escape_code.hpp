add7dc375550bf61e52b703711995b103e87771b
SupasanKomonlit <supasan_224@hotmail.com>
2019-04-06 12:53:15 +0700

    Move variable to header

--- zeabus/include/zeabus/escape_code.hpp
+++ zeabus/include/zeabus/escape_code.hpp
@@ -1,4 +1,4 @@
-// FILE         : escape_sequence.hpp
+// FILE         : escape_code.hpp
 // AUTHOR       : Supasan Komonlit
 // CREATE ON    : 2019, APRIL 05
 
@@ -13,14 +13,35 @@
 //  : http://ascii-table.com/ansi-escape-sequences.php
 //  : https://stackoverflow.com/questions/37774983/clearing-the-screen-by-printing-a-character
 
+#ifndef _ZEABUS_ESCAPE_CODE_HPP__
+#define _ZEABUS_ESCAPE_CODE_HPP__
+
 namespace zeabus
 {
 
 namespace escape_code
 {
 
+    const static std::string normal_red = "\x1B[0;31m";
+    const static std::string normal_greem = "\x1B[0;32m";
+    const static std::string normal_yellow = "\x1B[0;33m";
+    const static std::string normal_blue = "\x1B[0;34m";
+    const static std::string normal_margenta = "\x1B[0;35m";
+    const static std::string normal_cyan = "\x1B[0;36m";
+    const static std::string normal_white = "\x1B[0;37m";
+
+    const static std::string bold_red = "\x1B[1;31m";
+    const static std::string bold_greem = "\x1B[1;32m";
+    const static std::string bold_yellow = "\x1B[1;33m";
+    const static std::string bold_blue = "\x1B[1;34m";
+    const static std::string bold_margenta = "\x1B[1;35m";
+    const static std::string bold_cyan = "\x1B[0;36m";
+    const static std::string bold_white = "\x1B[1;37m";
+
     void clear_screen();
 
 } // namespace escape_code
 
 } // namespace zeabus
+
+#endif // _ZEABUS_ESCAPE_CODE_HPP__
--- zeabus/src/zeabus/escape_code.cpp
+++ zeabus/src/zeabus/escape_code.cpp
@@ -1,4 +1,4 @@
-// FILE         : escape_sequence.cpp
+// FILE         : escape_code.cpp
 // AUTHOR       : Supasan Komonlit
 // CREATE ON    : 2019, APRIL 05
 
@@ -9,22 +9,6 @@ namespace zeabus
 
 namespace escape_code
 {
-
-    const static std::string normal_red = "\x1B[0;31m";
-    const static std::string normal_greem = "\x1B[0;32m";
-    const static std::string normal_yellow = "\x1B[0;33m";
-    const static std::string normal_blue = "\x1B[0;34m";
-    const static std::string normal_margenta = "\x1B[0;35m";
-    const static std::string normal_cyan = "\x1B[0;36m";
-    const static std::string normal_white = "\x1B[0;37m";
-
-    const static std::string bold_red = "\x1B[1;31m";
-    const static std::string bold_greem = "\x1B[1;32m";
-    const static std::string bold_yellow = "\x1B[1;33m";
-    const static std::string bold_blue = "\x1B[1;34m";
-    const static std::string bold_margenta = "\x1B[1;35m";
-    const static std::string bold_cyan = "\x1B[0;36m";
-    const static std::string bold_white = "\x1B[1;37m";
 
     void clear_screen()
     {
