16a65e97cae940ffdbd257555d080da28adfd7b6
SupasanKomonlit <supasan_224@hotmail.com>
2019-04-08 00:08:42 +0700

    Add header file connect with dvl

--- zeabus/include/zeabus/sensor/DVL/connector.hpp
+++ zeabus/include/zeabus/sensor/DVL/connector.hpp
@@ -6,4 +6,89 @@
 
 #include    <zeabus/serial/synchronous_port.cpp> // Use to connect DVL
 
+// DVL use string to set or connect with hardware 
+// we reference data for ExplorerDVL Operation Manual.pdf
 
+namespace zeabus
+{
+
+namespace sensor
+{
+
+namespace DVL
+{
+
+    class connector : public zeabus::serial::SynchronousPort
+    {
+
+        public:
+            connector( std::string port_name = "" );
+
+            bool set_idle(); // use this for stop streaming
+
+            // set number of bottom-track pings to average together in each data ensemble
+            // Page 119 format BPnnn 
+            //      nnn = 0 to 999 pings
+            // set number of bottom-track pings to average together before sending/recording
+            bool bottom_track( std::string data = "001" );
+            
+            // Page 120 : set maximum tracking depth in decimeters unit ( meters * 10 )
+            // example is depth in area deployment is 20 meter  set 210 metes
+            bool max_depth( std::string data = "01000");
+
+            // Page 133 : set heading of sensor to forward robot 
+            // look number at hardward  4 to 3 is Y axis to x axis of robot
+            // and use ENU to position or negative
+            // format EAsnnnnn
+            //      s is <+> , <->
+            //      nnnnn = 00000 to 18000
+            //      snnnnn = -17999 to 18000 
+            bool set_heading( std::string data = "00000" ); 
+        
+            // Page 134 : set the water salinty value
+            // format ESnn
+            //      nn = 00 to 40
+            bool set_salinty( std::string data = "35" );
+
+            // Page 163 : set time per ensemble
+            // format TEhh:mm::ss.ff
+            //      hh = 00 to 23 hours
+            //      mm = 00 to 59 minutes
+            //      ss = 00 to 59 seconds
+            //      ff = 00 to 99 hundredths of second
+            // Default TE00:00:00.00
+            //      Default that mean if success collect data will continue start collecting next
+            bool time_per_ensemble( std::string data = "00:00:00.00" );
+
+            // Page 164 : set time between Pings warning this have affect to command TE
+            // format TPmm:ss.ff
+            //      mm = 00 to 59 minutes
+            //      ss = 00 to 59 seconds
+            //      ff = 00 to 99 hundredths of second
+            // Increase if (WP x TP ) > TE
+            bool time_between_pings( std::string data = "00:00.05");
+
+            // Page 167 : set pings per ensumble
+            // format WPnnnnn
+            //      nnnnn = 0 to 16384 pings
+            // if WP is zero the DVL dose not collect water profile data
+            //    WP will extends if (WP + BP) x TP > TE
+            bool ping_per_ensemble( std::string data = "00000" );
+
+            // Page 156 : Choose data stream select
+            bool data_stream( std::string data = "6" );
+    
+            // Page 128 : CK keep parameters
+            // Manual have suggestion about send command CR1 first and CK before CS<data stream>
+            bool save_paramter();
+            
+            // Page 129 : CS Starting pinging 
+            bool resume();
+
+    }; // class connector
+ 
+} // namespace
+
+} // namespace sensor
+
+} // namespace zeabus
