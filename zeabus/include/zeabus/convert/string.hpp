ec687cf50acb7e746b7e81f7ff4d725bf0c92ab9
SupasanKomonlit <supasan_224@hotmail.com>
2019-04-09 02:09:33 +0700

    Add convert string to integer

---
+++ zeabus/include/zeabus/convert/string.hpp
@@ -0,0 +1,26 @@
+// File         : string.hpp
+// Author       : Supasan Komonlit
+// Create on    : 2019, APRIL 9
+
+#include    <iostream>
+
+// This file header have purpose to include file for convert string to another type
+// Type available 
+//      interger
+
+namespace zeabus
+{
+
+namespace convert
+{
+
+namespace string
+{
+
+    bool to_integer( std::string* data , int* result );
+
+}
+
+} // namespace convert
+
+} // namespace zeabus
