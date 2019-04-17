ad4ade1e49dd75713f2b41ace99f266425e61cce
SupasanKomonlit <supasan_224@hotmail.com>
2019-04-18 02:02:58 +0700

    Add header

---
+++ zeabus/include/zeabus/service/get_data/sensor_imu.hpp
@@ -0,0 +1,54 @@
+// FILE         : sensor_imu.hpp
+// AUTHOR       : Supasan Komonlit
+// CREATE ON    : 2019 , APRIL 18
+
+// This file is service node for send data about imu
+//  message is sensor_msg/msg/Imu
+//  Follow structure in example_code of ros2 minimal_subscriber package 
+//      member_function.cpp file
+
+#include    "rclcpp/rclcpp.hpp"
+
+#include    <iostream>
+
+#include    "sensor_msg/msg/imu.hpp"
+
+#include    "zeabus/srv/get_sensor_imu.hpp"
+
+#include    <memory>
+
+#ifndef _ZEABUS_SERVICE_GET_DATA_SENSOR_IMU_HPP__
+#define _ZEABUS_SERVICE_GET_DATA_SENSOR_IMU_HPP__
+
+namespace zeabus
+{
+namespace service
+{
+namespace get_data
+{
+    class SensorImu : public rclcpp::Node
+    {
+        public:
+            SensorImu( std::string node_name ); // this function will init node together
+
+            void regis_data( sensor_msgs::msg::Imu* pointer_data );
+
+            rclcpp::Service< zeabus::srv::GetSensorImu >::SharedPtr service_variable;
+
+        private:
+            void callback( 
+                    const std::shared_ptr< rmw_request_id_t > request_header 
+                    , const std::shared_ptr< zeabus::srv::GetSensorImu::Request > request
+                    , const std::shared_ptr< zeabus::srv::GetSensorImu::Response > response );
+
+            sensor_msgs::msg::Imu* pointer_data;
+
+    }; // class sensor_imu 
+
+} // namespace get_data
+
+} // namespace service
+
+} // namespace zeabus
+
+#endif
