2a0fe7571cf3ab305143c53b1de7811342c58e58
Phitchawat Lukkanathiti <me@shayennn.com>
2019-04-20 22:51:15 +0700

    Destroy the image lib!

--- zeabus_vision/gate/finder.py
+++ zeabus_vision/gate/finder.py
@@ -6,27 +6,44 @@
     Python Version: 3.6
 """
 
+import numpy as np
+import cv2 as cv
+
 import rclpy
 from sensor_msgs.msg import CompressedImage
 from zeabus.srv import VisionGate
 from gate.gate import Gate
-from gate.image import Image
+
 
 g_node = None
+SUB_SAMPLING = 0.3
 PUBLIC_TOPIC = '/vision/mission/gate'
+CAMERA_TOPIC = '/vision/front/image_rect_color/compressed'
 DEBUG = {
     'console': True,
     'imageSource': True
 }
 
 process_obj = Gate()
-image = Image(DEBUG['imageSource'])
-last_found = [0, 0, 0, 0, 0]
+image = None
+last_found = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+
+def imageCallback(msg):
+    global image, SUB_SAMPLING
+    arr = np.fromstring(msg.data, np.uint8)
+    image = cv.resize(cv.imdecode(arr, 1), (0, 0),
+                      fx=SUB_SAMPLING, fy=SUB_SAMPLING)
 
 
 def find_gate():
     global image, last_found
-    output = process_obj.doProcess(image.get())
+    output = None
+    if image is not None:
+        output = process_obj.doProcess(image)
+        output = [float(i) for i in output]
+    else:
+        print('No input image')
     if output is not None:
         last_found = output
         return [1]+last_found
@@ -38,9 +55,9 @@ def gate_callback(msg, res):
     global g_node
     task = str(msg.task.data)
     req = str(msg.req.data)
-    if DEBUG['console']:
-        print(task, req)
-    if task in ['gate']:
+    if DEBUG['console'] or task == '':
+        print('Service called', msg, req)
+    if task in ['gate', '']:
         find_result = find_gate()
         res.found = find_result[0]
         res.cx1 = find_result[1]
@@ -49,12 +66,14 @@ def gate_callback(msg, res):
         res.x_right = find_result[4]
         res.area = find_result[5]
     else:
-        res.found = -1
-        res.cx1 = 0
-        res.cy1 = 0
-        res.x_left = 0
-        res.x_right = 0
-        res.area = 0
+        if DEBUG['console'] or task == '':
+            print('Not OK')
+        res.found = 2
+        res.cx1 = 0.0
+        res.cy1 = 0.0
+        res.x_left = 0.0
+        res.x_right = 0.0
+        res.area = 0.0
     return res
 
 
@@ -65,7 +84,7 @@ def main(args=None):
     g_node = rclpy.create_node('vision_gate')
 
     sub = g_node.create_subscription(
-        CompressedImage, image.topic('front'), image.callback)
+        CompressedImage, CAMERA_TOPIC, imageCallback)
 
     srv = g_node.create_service(VisionGate, 'gate_service', gate_callback)
     while rclpy.ok():
